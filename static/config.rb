5cdcc28567dc8200ed5e0ba1093b7922308f4691
sinobasedm <siler_shen@sinobasedm.com>
2016-11-29 16:58:57 +0800

    project init

---
+++ static/config.rb
@@ -0,0 +1,17 @@
+# Set this to the root of your project when deployed:
+encoding = "utf-8"
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascript_dir = "javascript"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style =:compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
