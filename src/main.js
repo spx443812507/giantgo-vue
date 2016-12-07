1bf86322e394c060461db32482f7b8c05357e1f7
sinobasedm <siler_shen@sinobasedm.com>
2016-12-07 14:07:03 +0800

    import mint-ui

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import VueRouter from 'vue-router'
 import routes from './routers'
 import store from './store'
 import App from './App'
+import { Cell, Checklist, Button } from 'mint-ui'
 
+Vue.component(Cell.name, Cell)
+Vue.component(Checklist.name, Checklist)
+Vue.component(Button.name, Button)
 Vue.use(VueRouter)
 
 /* eslint-disable no-new */
